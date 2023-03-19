c37cd6cf609d03d2ee40316f8e8a59b45078120a
AishaMalik <60092058@cna-qatar.edu.qa>
2023-03-19 18:08:14 +0300

    mistake

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask_restful import Api
 from resources.main_menu import MainMenu
 from resources.recipes_crud import Recipes
-from resources.recipes_crud import RecipesImport
-
 
 
 app = Flask(__name__)
@@ -11,7 +9,6 @@ api = Api(app)
 
 api.add_resource(MainMenu, "/")
 api.add_resource(Recipes, "/recipes")
-api.add_resource(RecipesImport, "/import")
 
 
 if __name__ == '__main__':
