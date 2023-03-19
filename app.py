eb736d5adf5b9bebd769e9237dfe862b71eb9c92
Hassan-60084538 <113988484+Hassan-60084538@users.noreply.github.com>
2023-03-19 17:43:52 +0300

    add recipes path to app file

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_restful import Api
 from resources.main_menu import MainMenu
 from resources.recipes_crud import Recipes
+from resources.recipes_crud import RecipesImport
+
 
 
 app = Flask(__name__)
@@ -9,6 +11,7 @@ api = Api(app)
 
 api.add_resource(MainMenu, "/")
 api.add_resource(Recipes, "/recipes")
+api.add_resource(RecipesImport, "/import")
 
 
 if __name__ == '__main__':
