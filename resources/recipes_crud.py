617184d916ae276024f3e7801f5e0b56eb0ac24e
nikaNahar919 <127874125+nikaNahar919@users.noreply.github.com>
2023-03-22 19:01:51 +0300

    Squashed commit of the following:
    commit 6e5c0484da03a5b223cd01144036c42384001cc3
    Author: zainab33 <zali@binyousef.com>
    Date:   Wed Mar 22 18:01:30 2023 +0300
    
        add replaced functions
    
    commit bb92754099dfdb1a48861001d79569415bc27677
    Merge: 67c2fc4 0369ddf
    Author: Merina Faruk <60096964@cna-qatar.edu.qa>
    Date:   Wed Mar 22 17:43:51 2023 +0300
    
        Merge branch 'merina'
    
    commit 0369ddfcd70ffd6ed17ccced6e483bda0ff4ef69
    Merge: ae77a21 04ed5a6
    Author: Merina Faruk <60096964@cna-qatar.edu.qa>
    Date:   Wed Mar 22 17:43:07 2023 +0300
    
        Merge branch 'master' into merina
    
    commit 67c2fc4d1b6dd706da02ef5563cffc83b2570524
    Author: Zainab Ali <77604160+zaali33@users.noreply.github.com>
    Date:   Wed Mar 22 17:38:32 2023 +0300
    
        add __pycache__ to gitignore
    
    commit ae77a214e2c47de29667ad81b8a907d7f7e232c5
    Author: Merina Faruk <60096964@cna-qatar.edu.qa>
    Date:   Wed Mar 22 17:20:38 2023 +0300
    
        Record changes to the repository
    
    commit 04ed5a6aa3d8e6822bab94389d1e28b12b2a4b7e
    Author: Aahmad <60093464@cna-qatar.edu.qa>
    Date:   Tue Mar 21 22:12:54 2023 +0300
    
        added validation for add recipe
    
    commit 9775c2206d199523b5b4e0911fd4a44a42f6f219
    Merge: b0b8054 300dd34
    Author: AahmadAA <74532220+AahmadAA@users.noreply.github.com>
    Date:   Tue Mar 21 18:28:16 2023 +0300
    
        Merge pull request #2 from zaali33/addRecipe
    
        Add recipe
    
    commit 300dd343d8464bfb78b9760db942ad82e6c86c9f
    Merge: d7f395b b0b8054
    Author: AahmadAA <74532220+AahmadAA@users.noreply.github.com>
    Date:   Tue Mar 21 18:28:04 2023 +0300
    
        Merge branch 'master' into addRecipe
    
    commit b0b80545782e7eb08d32fe4f988c7c465513c375
    Merge: 79c0e0d 6cc54cf
    Author: Aisha Shahzad <60092058@cna-qatar.edu.qa>
    Date:   Tue Mar 21 18:19:44 2023 +0300
    
        Merge branch 'master' of https://github.com/zaali33/recipes_project
    
    commit 79c0e0d8957fe2af5567d100a6ece1d14a035a19
    Author: Aisha Shahzad <60092058@cna-qatar.edu.qa>
    Date:   Tue Mar 21 18:10:03 2023 +0300
    
        commit issue fixed
    
    commit 1a162d251015513da80f0fb33aefe5230bd26f1c
    Author: Aisha Shahzad <60092058@cna-qatar.edu.qa>
    Date:   Tue Mar 21 18:08:42 2023 +0300
    
        fix issue:
    
    commit df0436c84f100f8ea57786f8df726a5f5384e29c
    Author: Aisha Shahzad <60092058@cna-qatar.edu.qa>
    Date:   Tue Mar 21 18:07:19 2023 +0300
    
        untrack file error
    
    commit d7f395bfca3de3161e912f70a585e9eb34e72dc8
    Author: Aahmad <60093464@cna-qatar.edu.qa>
    Date:   Tue Mar 21 17:32:23 2023 +0300
    
        fixed successfully added page
    
    commit 6cc54cf8eed6ca209a453dc4ea184018cd586fc8
    Author: Hanan Mohammad asif <hananasif@iHorizonss-MacBook-Air.local>
    Date:   Tue Mar 21 17:32:19 2023 +0300
    
        edit feature compeleted
    
    commit 25773323a307a3ec4b599a0687f85fd9e1a57c2a
    Author: Hanan Mohammad asif <hananasif@iHorizonss-MacBook-Air.local>
    Date:   Tue Mar 21 17:31:59 2023 +0300
    
        done with edit function
    
    commit c0a412bca65a8c2d20873910b20933caff497d49
    Author: Hanan Mohammad asif <hananasif@iHorizonss-MacBook-Air.local>
    Date:   Tue Mar 21 17:30:24 2023 +0300
    
        added recripe management
    
    commit 9cae13c59b3f14e99e56e786b2e75d7abcee84cb
    Author: Aahmad <60093464@cna-qatar.edu.qa>
    Date:   Mon Mar 20 22:50:51 2023 +0300
    
        added functionality for add recipe to add to json file
    
    commit 4b5aee1da0672d185924a0973f3ed384e32ecdda
    Merge: fe40c51 57ca99e
    Author: Aahmad <60093464@cna-qatar.edu.qa>
    Date:   Mon Mar 20 22:18:49 2023 +0300
    
        Merge branch 'master' into addRecipe
    
    commit fe40c514f6432ef17916c4eddccd347da60e0cbe
    Author: Aahmad <60093464@cna-qatar.edu.qa>
    Date:   Mon Mar 20 21:51:44 2023 +0300
    
        added function for add recipe
    
    commit 57ca99e664119ec95b577466f5c4b37dd21f7005
    Author: Aisha Shahzad <60092058@cna-qatar.edu.qa>
    Date:   Sun Mar 19 21:09:50 2023 +0300
    
        make changes in import function to accept only .json files

--- resources/recipes_crud.py
+++ resources/recipes_crud.py
@@ -1,7 +1,18 @@
 from flask_restful import Resource
-from flask import Response, render_template
-from functions.recipe_management import RecipeManagement
+from flask import Response, render_template, redirect, request
+from functions.recipe_management import RecipeManagement, Recipe
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, IntegerField, HiddenField
+from wtforms.validators import DataRequired, NumberRange, InputRequired
 
+
+class RecipeForm(FlaskForm):
+    id = HiddenField()
+    name = StringField(label="Name", validators=[DataRequired()])
+    description = TextAreaField(label="Description", validators=[DataRequired()])
+    image_url = StringField(label="Image URL", validators=[DataRequired()])
+    category = StringField(label="Category", validators=[DataRequired()])
+    rating = IntegerField(label="Rating", validators=[NumberRange(1, 5)])
 
 # view (get) and add (post) functions will be called in this class
 class Recipes(Resource):
@@ -16,6 +27,65 @@ class Recipes_export(Resource):
         recipe_management = RecipeManagement()
         return recipe_management.export_recipes()
     
+class RecipeAdd(Resource):
+    def get(self):
+        recipe_form = RecipeForm()
+        return Response(response=render_template("add-recipe.html", recipe_form=recipe_form))
+    
+    def post(self):
+        # recipe = Recipe(id,name=request.form['recipeName'],
+        #                 #   ingredients=request.form['recipeIngredients'],
+        #                   description=request.form['recipeInstructions'],
+        #                   category=request.form['recipeCategory'],
+        #                   rating=request.form['recipeRating'],
+        #                   image_url=request.form['recipeImage'])
+
+        # recipe_management = RecipeManagement()
+        # result = recipe_management.add_recipe(recipe)
+        # return Response(response=render_template("added-success.html", result = result))
+    
+        recipe_form = RecipeForm()
+        recipe_management = RecipeManagement()
+        if recipe_form.validate_on_submit():
+            recipe = Recipe(  
+            id,name=recipe_form.data.get("name"),
+        #   ingredients=request.form['recipeIngredients'],
+            description=recipe_form.data.get("description"),
+            category=recipe_form.data.get("category"),
+            rating=recipe_form.data.get("rating"),
+            image_url=recipe_form.data.get("image_url"))          
+            recipe_management.add_recipe(recipe)
+            return redirect("/recipes")
+        else:
+            # recipe = recipe_management.get_recipe(recipe_form.data.get('id'))
+            return Response(response=render_template('add-recipe.html', recipe_form=recipe_form))
+
+class RecipesEdit(Resource):
+    def get(self, id):
+        recipe_management = RecipeManagement()
+        recipe = recipe_management.get_recipe(id)
+        recipe_form = RecipeForm()
+        if recipe:
+            recipe_form.id.data = recipe["id"]
+            recipe_form.name.data = recipe["name"]
+            recipe_form.description.data = recipe["description"]
+            recipe_form.image_url.data = recipe["image_url"]
+            recipe_form.category.data = recipe["category"]
+            recipe_form.rating.data = recipe["rating"]
+        return Response(response=render_template("edit_recipe.html", recipe=recipe, recipe_form=recipe_form))
+
+    def post(self):
+        recipe_form = RecipeForm()
+        recipe_management = RecipeManagement()
+        if recipe_form.validate_on_submit():
+            recipe_management.edit_recipe(int(recipe_form.data.get('id')), recipe_form.data.get("name"),
+                                          recipe_form.data.get("description"), recipe_form.data.get("category"),
+                                          recipe_form.data.get("rating"), recipe_form.data.get("image_url"))
+            return redirect("/recipes")
+        else:
+            recipe = recipe_management.get_recipe(recipe_form.data.get('id'))
+            return Response(response=render_template('edit_recipe.html', recipe=recipe, recipe_form=recipe_form))
+
 class RecipesImport(Resource):
     def get(self):
         return Response(response=render_template("import-recipes.html"))
@@ -23,5 +93,15 @@ class RecipesImport(Resource):
     def post(self):
         recipe_management = RecipeManagement()
         result = recipe_management.import_recipes()
-        return Response(response=render_template("import-success.html", result = result))
+        return Response(response=render_template("import-success.html", result=result))
 
+
+class RecipeDelete(Resource):
+    def get(self):
+        recipe_management = RecipeManagement()
+        id = request.args.get("id")
+        try:
+            recipe_management.delete_recipe(id)
+        except:
+            return redirect("/recipes")
+        return redirect("/recipes")
